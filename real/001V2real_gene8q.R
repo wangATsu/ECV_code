e82a091f709aaddf865b145d67d42867fbeb1913
wangATsu <wangkingston916@gmail.com>
2025-10-26 17:54:06 +0800

    Update 001V2real_gene8q.R

--- real/001V2real_gene8q.R
+++ real/001V2real_gene8q.R
@@ -175,12 +175,10 @@ for (n in c(707)) {
       }
       
       # Determine row index for results (only K=5 case)
-      # if (K == 2) r <- 1  # COMMENTED OUT
-      # else
       #if (K == 5) 
-        r <- 2  # COMMENTED OUT
-     # else if (K == 10)  r <- 3  # COMMENTED OUT
-      # r <- 3 # MODIFIED: Directly set r = 2 for K = 5
+        r <- 2  
+
+
       
       # Calculate different penalty methods
       es_loss <- loss_values[1, ]  # ES (first fold)
